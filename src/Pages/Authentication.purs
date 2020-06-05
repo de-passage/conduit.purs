c177c3f90fffeab018d718386d62b243afc8b3df
Sylvain Leclercq <maisbiensurqueoui@gmail.com>
2020-06-05 14:15:26 +0800

    Fix authentication view

--- src/Pages/Authentication.purs
+++ src/Pages/Authentication.purs
@@ -1,23 +1,21 @@
 module Pages.Authentication where
 
 import Prelude
-
 import API as API
 import Classes as C
+import Data.Array (snoc)
 import Data.Const (Const)
 import Data.Either (Either, either)
 import Data.Maybe (Maybe(..))
 import Data.Traversable (sequence)
 import Data.User (User, storeUser)
 import Effect.Aff.Class (class MonadAff)
-import Effect.Class.Console (log)
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 import Halogen.Themes.Bootstrap4 as BS
 import Router (loginUrl, registerUrl)
-import Unsafe.Coerce (unsafeCoerce)
 
 type Query
   = Const Void
@@ -75,32 +73,34 @@ initialState action =
 render :: forall m. State -> HH.ComponentHTML Action ChildSlots m
 render state =
   let
-    formContent text =
-      [ HH.fieldset [ HP.class_ BS.formGroup ]
-          [ HH.input
-              [ HP.classes [ BS.formControl, BS.formControlLg ]
-              , HP.type_ HP.InputEmail
-              , HP.placeholder "Email"
-              , HE.onValueChange (Just <<< EmailChanged)
-              ]
-          ]
-      , HH.fieldset [ HP.class_ BS.formGroup ]
-          [ HH.input
-              [ HP.classes [ BS.formControl, BS.formControlLg ]
-              , HP.type_ HP.InputPassword
-              , HP.placeholder "Password"
-              , HE.onValueChange (Just <<< PasswordChanged)
-              ]
-          ]
-      , HH.button
-          [ HP.classes [ BS.btn, BS.btnLg, BS.btnPrimary, C.pullXsRight ]
-          , HE.onClick (\_ -> Just PostRequest)
-          ]
-          [ HH.text text
-          ]
-      ]
+    fieldset input placeholder action =
+      HH.fieldset [ HP.class_ BS.formGroup ]
+        [ HH.input
+            [ HP.classes [ BS.formControl, BS.formControlLg ]
+            , HP.type_ input
+            , HP.placeholder placeholder
+            , HE.onValueChange (Just <<< action)
+            , HP.id_ placeholder
+            ]
+        ]
 
-    content text redirectMessage =
+    namefield = fieldset HP.InputText "Name" NameChanged
+
+    emailfield = fieldset HP.InputEmail "Email" EmailChanged
+
+    passwordfield = fieldset HP.InputPassword "Password" PasswordChanged
+
+    button text =
+      HH.button
+        [ HP.classes [ BS.btn, BS.btnLg, BS.btnPrimary, C.pullXsRight ]
+        , HE.onClick (\_ -> Just PostRequest)
+        ]
+        [ HH.text text
+        ]
+
+    formContent text fields = fields `snoc` button text
+
+    content text fields redirectMessage =
       HH.div [ HP.class_ C.authPage ]
         [ HH.div [ HP.classes [ BS.container, C.page ] ]
             [ HH.div [ HP.class_ BS.row ]
@@ -111,15 +111,19 @@ render state =
                         ]
                     , HH.ul [ HP.class_ C.errorMessages ]
                         (map (\err -> HH.li_ [ HH.text err ]) state.errorMessages)
-                    , HH.form_ (formContent text)
+                    , HH.form_ (formContent text fields)
                     ]
                 ]
             ]
         ]
   in
     case state.action of
-      Login -> content "Sign in" $ HH.a [ HP.href registerUrl ] [ HH.text "Do not have an account yet?" ]
-      Register -> content "Sign up" $ HH.a [ HP.href loginUrl ] [ HH.text "Have an account already?" ]
+      Login ->
+        content "Sign in" [ emailfield, passwordfield ]
+          $ HH.a [ HP.href registerUrl ] [ HH.text "Do not have an account yet?" ]
+      Register ->
+        content "Sign up" [ namefield, emailfield, passwordfield ]
+          $ HH.a [ HP.href loginUrl ] [ HH.text "Have an account already?" ]
 
 handleAction ∷
   forall o m.
@@ -129,18 +133,14 @@ handleAction ∷
 handleAction = case _ of
   ActionChanged action -> H.modify_ (_ { action = action })
   PostRequest -> do
-    state <- H.get
+    state <- H.modify (_ { errorMessages = [] })
     case state.action of
       Login -> case sequence [ state.email, state.password ] of
-        Just [ email, password ] -> 
-          do 
-            user <- login email password
-            H.liftEffect $ log $ unsafeCoerce user
-            either
-              (H.modify_ <<< (\v -> _ { errorMessages = [ v ]}))
-              (H.liftEffect <<< storeUser)
-              user
-          
+        Just [ email, password ] -> do
+          user <- login email password
+          user # either
+            (H.modify_ <<< (\v -> _ { errorMessages = [ v ] }))
+            (H.liftEffect <<< storeUser)
         _ -> pure unit
       Register -> case sequence [ state.name, state.email, state.password ] of
         Just [ name, email, password ] -> pure unit
@@ -148,7 +148,6 @@ handleAction = case _ of
   NameChanged name -> H.modify_ _ { name = Just name }
   EmailChanged email -> H.modify_ _ { email = Just email }
   PasswordChanged password -> H.modify_ _ { password = Just password }
-
-  where 
-    login :: String -> String -> H.HalogenM State Action ChildSlots o m (Either String User)
-    login email password = H.liftAff $ API.login { email, password }
+  where
+  login :: String -> String -> H.HalogenM State Action ChildSlots o m (Either String User)
+  login email password = H.liftAff $ API.login { email, password }
